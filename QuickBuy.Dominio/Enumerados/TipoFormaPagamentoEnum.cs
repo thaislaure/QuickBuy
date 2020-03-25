42686c4602c3e304f4a07a875943092d1f20e9ca
Thais Laure <thais_laure@hotmail.com>
2020-03-24 22:37:12 -0400

    Criação das Entidades

---
+++ QuickBuy.Dominio/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Enumerados
+{
+    public enum TipoFormaPagamentoEnun
+    {
+        NaoDefinido = 0,
+        Boleto = 1,
+        CartaoCredito = 2,
+        Deposito = 3
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,30 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnun.Boleto; }
+        }
+
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnun.Deposito; }
+        }
+
+        public bool NãoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnun.NaoDefinido; }
+        }
+
+    }
+}
