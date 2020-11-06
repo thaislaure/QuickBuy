31dbb1ee18ac6c941b764ae76366f85db03088c8
Thais Laure <thais_laure@hotmail.com>
2020-11-05 22:01:24 -0400

    Antes do Merge

---
+++ QuickBuy.Web/ClientApp/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Usuario } from '../model/usuario';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  public usuario;
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+
+  }
+}
